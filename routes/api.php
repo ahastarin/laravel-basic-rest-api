576ea48c05d0a240e4379681cc5a096bc33172c1
ahastarin <ahastarin@gmail.com>
2023-01-15 16:38:56 +0700

    update controller

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,10 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisterController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Auth\SignInController;
 use Illuminate\Support\Facades\Route;
-use App\Providers\RouteServiceProvider;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 
 Route::post('register', RegisterController::class);
+Route::post('login', SignInController::class);
