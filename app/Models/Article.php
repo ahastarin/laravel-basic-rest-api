1b16b6c67a09e8745c8ec092832dcf3c88559a8d
ahastarin <ahastarin@gmail.com>
2023-01-16 17:57:10 +0700

    setting table article & subject

---
+++ app/Models/Article.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    protected $fillable=["title", "slug", "body", "subject_id"];
+
+    public function user()
+    {
+        $this->belongsTo(User::class);
+    }
+
+    public function subject()
+    {
+        $this->belongsTo(Subject::class);
+    }
+}
